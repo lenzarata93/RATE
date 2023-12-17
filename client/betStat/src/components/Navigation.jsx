67e15b9fa7cd836ae2af733a8be222bf99f45a97
lenzarata93 <117170826+lenzarata93@users.noreply.github.com>
2023-12-17 20:25:25 +0200

    Update Navigation.jsx

--- client/betStat/src/components/Navigation.jsx
+++ client/betStat/src/components/Navigation.jsx
@@ -2,13 +2,13 @@ import styles from '../components/Navigation.module.css'
 function Navigation(){
     return( <nav>
         <ul>
-          <li><a href="#">Начало</a></li>
-          <li><a href="#">Регистрация</a></li>
-          <li><a href="#">Вход</a></li>
-          <li><a href="#">Изход</a></li>
-          <li><a href="#">Движения</a></li>
-          <li><a href="#">Моят профил</a></li>
-          <li><a href="#">Моята статистика</a></li>
+          <li><a href="/">Начало</a></li>
+          <li><a href="/registration">Регистрация</a></li>
+          <li><a href="/login">Вход</a></li>
+          <li><a href="/logout">Изход</a></li>
+          <li><a href="/moves">Движения</a></li>
+          <li><a href="/myProfile">Моят профил</a></li>
+          <li><a href="/myStat">Моята статистика</a></li>
         </ul>
       </nav>
     );
