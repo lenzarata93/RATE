60d4d7ebfffd640f93ebd80d80f3dbf6650316e2
lenzarata93 <117170826+lenzarata93@users.noreply.github.com>
2024-01-02 10:11:19 +0200

    ading DataBase

--- server/DataBase/dataBase.js
+++ server/DataBase/dataBase.js
@@ -1,1 +1,10 @@
 const mongoose = require('mongoose');
+
+const uri = 'mongodb://127.0.0.1:27017/MyCasinoStatApp';
+
+async function dataBaseConnect(){
+
+    await mongoose.connect(uri);
+};
+
+module.exports = dataBaseConnect;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 const express = require(`express`);
+const DB = require('./DataBase/dataBase');
+const dataBaseConnect = require('./DataBase/dataBase');
 const app = express();
 const PORT = 4000;
+
+dataBaseConnect()
+.then(()=>{
+    console.log(`Data Base is here`);
+})
+.catch(err=>{
+    console.log(`Data Base error : ${err}`)
+});
+
 
 
 app.get('/', (req, res) => {
